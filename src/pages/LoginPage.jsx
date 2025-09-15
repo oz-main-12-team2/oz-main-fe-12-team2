d91ace9cb68f5db494351ee9fcbc4809b69430cd
Justin-SJ-Hong <41540100+Justin-SJ-Hong@users.noreply.github.com>
2025-09-15 17:40:18 +0900

    Feat: 로그인 페이지 UI 완료 (#10)
    * WIP: 로그인 화면 임시 저장
    
    * Feat: 로그인 페이지 UI 완료
    
    * Fix: 로그인페이지 로고 크기 및 여백 수정, 소셜로그인 타이틀 제거

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,64 @@
+import '../styles/loginpage.scss'
+import FormGroup from '../components/common/FormGroup';
+import Button from '../components/common/Button';
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import naverlogin from '../assets/btnG_complete_login.png';
+import googlelogin from '../assets/web_neutral_sq_SI@4x.png';
+
 function LoginPage() {
+    const [showPassword, setShowPassword] = useState(false);
     return (
-        <h2>로그인 페이지</h2>
+        <div className="base-container">
+            <img className="login-logo" src="../../public/logo.svg" alt="러블리 로고" />
+
+            <div className='login-container'>
+                <FormGroup type="email" placeholder="이메일" />
+                <div className='password-input'>
+                    <FormGroup type={showPassword ? "text" : "password"} placeholder="비밀번호" />
+                    {showPassword ? (
+                        <FaRegEye
+                            className='login-eye-icon'
+                            role='button'
+                            aria-label='비밀번호 가리기'
+                            tabIndex={0}
+                            onClick={() => setShowPassword(false)}
+                        />
+                    ) : (
+                        <FaRegEyeSlash
+                            className='login-eye-icon'
+                            role='button'
+                            aria-label='비밀번호 보기'
+                            tabIndex={0}
+                            onClick={() => setShowPassword(true)}
+                        />
+                    )}
+                </div>
+
+                <div className='login-account-options'>
+                    <div className='login-button'>
+                        <Button variant='primary' size='lg' type='submit'>
+                            로그인
+                        </Button>
+                    </div>
+                    <div className='account-options'>
+                        <Link to="#">비밀번호 찾기</Link>
+                        <Link to="/signup">회원가입</Link>
+                    </div>
+                </div>
+
+                <div className='social-login'>
+                    <Link to="#" className='naver-login'>
+                        <img src={naverlogin} alt='네이버로그인' />
+                    </Link>
+                    <Link to="#" className='google-login'>
+                        <img src={googlelogin} alt='구글로그인' />
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
