1916df22fe9af2f23fe50a51039335ba3516811e
Justin-SJ-Hong <madwolves98@gmail.com>
2025-09-19 18:00:36 +0900

    마이페이지 임시 저장

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,12 @@
 import '../styles/mypage.scss'
+import { Outlet } from 'react-router-dom';
 import { MdOutlineMailOutline, MdOutlineHome } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import Button from '../components/common/Button';
 import { Link } from "react-router-dom"
 import { useState } from 'react';
+import Header from '../components/layout/Header'
+import Footer from '../components/layout/Footer'
 import Modal from '../components/common/Modal';
 import FormGroup from '../components/common/FormGroup';
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
@@ -78,59 +81,82 @@ function MyPage() {
 
     return (
         <>
-            <div className='mypage-layout'>
-                <aside className="mypage-sidebar">
-                    <img className="mypage-logo" src="../../public/logo.svg" alt="러블리 로고" />
-                    <nav>
-                        <Link to="/mypage">
-                            <span>나의정보</span>
-                        </Link>
-                        <Link to="/mypage/cart">
-                            <span>장바구니</span>
-                        </Link>
-                    </nav>
-                </aside>
-                <div className="base-container">
-                    <div className='mypage-content'>
-                        <h1 className='mypage-title'>나의 정보</h1>
-                        <p className='mypage-greeting'>홍엽님 반갑습니다!</p>
-
-                        <div className='mypage-info'>
-                            <div className='mypage-email'>
-                                <MdOutlineMailOutline />
-                                <span>madwolves98@gmail.com</span>
-                            </div>
-                            <div className='mypage-address'>
-                                <MdOutlineHome />
-                                <span>서울시 마포구</span>
-                            </div>
-                        </div>
-                    </div>
+            <div className="base-container">
+                <Header />
+                <div className='mypage-layout'>
                     
-
-                    <div className='mypage-buttons'>
-                        <Button
-                            variant='primary'
-                            onClick={() => setIsEditDataModalOpen(true)}
-                        >
-                            개인정보 수정
-                        </Button>
-                        <Button
-                            variant='primary'
-                            onClick={() => setIsEditPasswordModalOpen(true)}
-                        >
-                            비밀번호 수정
-                        </Button>
-                        <Button
-                            variant='secondary'
-                            onClick={() => setDeleteMemberModalOpen(true)}
-                        >
-                            회원탈퇴
-                        </Button>
+                    <aside className="mypage-sidebar">
+                        <nav>
+                            <p className='mypage-title'>마이페이지</p>
+                            <img className="mypage-logo" src="../../public/logo.svg" alt="러블리 로고" />
+                            <div className='profile'>
+                                <p className='mypage-greeting'>홍엽님 반갑습니다!</p>
+                                {/* mypage-info의 경우 글자가 넘칠 경우 어떻게 할 지 */}
+                                <div className='mypage-info'>
+                                    <div className='mypage-email'>
+                                        <MdOutlineMailOutline />
+                                        <p>madwolves98@gmail.com</p>
+                                    </div>
+                                    <div className='mypage-address'>
+                                        <MdOutlineHome />
+                                        <p>서울시 마포구</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <Link to="/mypage">
+                                <span>나의정보</span>
+                            </Link>
+                            <Link to="/mypage/cart">
+                                <span>장바구니</span>
+                            </Link>
+                        </nav>
+                    </aside>
+                    <div className='mypage-main'>
+                        <Outlet />
                     </div>
                 </div>
+                <Footer />
 
-                {/* 개인정보 수정 모달 */}
+                {/* <div className='mypage-content'>
+                    <h1 className='mypage-title'>나의 정보</h1>
+                    <p className='mypage-greeting'>홍엽님 반갑습니다!</p>
+                    <div className='mypage-info'>
+                        <div className='mypage-email'>
+                            <MdOutlineMailOutline />
+                            <span>madwolves98@gmail.com</span>
+                        </div>
+                        <div className='mypage-address'>
+                            <MdOutlineHome />
+                            <span>서울시 마포구</span>
+                        </div>
+                    </div>
+                </div> */}
+                    
+
+                {/* <div className='mypage-buttons'>
+                    <Button
+                        variant='primary'
+                        onClick={() => setIsEditDataModalOpen(true)}
+                    >
+                        개인정보 수정
+                    </Button>
+                    <Button
+                        variant='primary'
+                        onClick={() => setIsEditPasswordModalOpen(true)}
+                    >
+                        비밀번호 수정
+                    </Button>
+                    <Button
+                        variant='secondary'
+                        onClick={() => setDeleteMemberModalOpen(true)}
+                    >
+                        회원탈퇴
+                    </Button>
+                </div> */}
+
+                
+            </div>
+            {/* <div className='mypage-layout'>
                 <Modal
                     isOpen={isEditDataModalOpen}
                     onClose={() => setIsEditDataModalOpen(false)}
@@ -175,7 +201,6 @@ function MyPage() {
                     <hr />
                 </Modal>
 
-                {/* 비밀번호 수정 모달 */}
                 <Modal
                     isOpen={isEditPasswordModalOpen}
                     onClose={() => setIsEditPasswordModalOpen(false)}
@@ -295,7 +320,6 @@ function MyPage() {
                     <hr />
                 </Modal>
                 
-                {/* 회원탈퇴 모달 */}
                 <Modal
                     isOpen={deleteMemeberModalOpen}
                     onClose={() => setDeleteMemberModalOpen(false)}
@@ -342,7 +366,7 @@ function MyPage() {
                     
                     <hr />
                 </Modal>
-            </div>
+            </div> */}
             
         </>
     )
