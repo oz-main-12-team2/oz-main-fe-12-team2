bb26ac077781e61504b05118838d94ad84afd344
mminseop <alstjq903@naver.com>
2025-09-24 18:34:23 +0900

    ✨ Feat: 관리자페이지 로그인 api 연동 완료

--- src/admin/pages/AdminLogin.jsx
+++ src/admin/pages/AdminLogin.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/admin.scss";
-// import { login } from "../../api/user";
 import Button from "../../components/common/Button";
 import FormGroup from "../../components/common/FormGroup";
 import { alertError, alertSuccess } from "../../utils/alert";
 import useUserStore from "../../stores/userStore";
+import { getUserMe, login } from "../../api/user";
 
 function AdminLogin() {
   const navigate = useNavigate();
@@ -13,39 +13,29 @@ function AdminLogin() {
   const [password, setPassword] = useState("");
   const [isloading, setIsLoading] = useState(false);
   const setUser = useUserStore((state) => state.setUser);
-
-  // 임시로 관리자계정
-  const adminData = {
-    email: "front-admin@naver.com",
-    password: "front-admin",
-  };
+  const getUser = useUserStore((state) => state.getUser);
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     try {
       setIsLoading(true);
-      //   const res = await login(email, password); // api 호출
+      const res = await login(email, password);
+      const test = await getUser();
+      console.log(test);
+      if (res.success) {
+        const checkAdmin = await getUserMe();
 
-      //   // 로그인 성공 후 확인..
-      //   if (res.data.is_admin) {
-      //     navigate("/admin", { replace: true });
-      //   } else {
-      //   await alertError(
-      //     "로그인 실패",
-      //     "이메일 또는 비밀번호가 올바르지 않습니다."
-      //   );
-      //   }      
-      if (email === adminData.email && password === adminData.password) {
-        // 성공 시 관리자 로그인 통과
-        setUser({ email: adminData.email, is_admin: true });
-        await alertSuccess("로그인 성공", "관리자 계정으로 로그인되었습니다.");
-        navigate("/admin", { replace: true });
-      } else {
-        await alertError(
-          "로그인 실패",
-          "이메일 또는 비밀번호가 올바르지 않습니다."
-        );
+        if (checkAdmin.is_admin) {
+          setUser(checkAdmin);
+          await alertSuccess(
+            "로그인 성공",
+            "관리자 계정으로 로그인되었습니다."
+          );
+          navigate("/admin", { replace: true });
+        } else {
+          await alertError("접근 불가", "관리자 계정만 접근 가능합니다.");
+        }
       }
     } catch (e) {
       await alertError("로그인 실패", e.message || "문제가 발생했습니다.");
